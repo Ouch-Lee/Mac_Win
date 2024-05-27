8e5a78fdf067e3fb043d7f70e5408de3a78bfab8
Ouch-Lee <84009206+Ouch-Lee@users.noreply.github.com>
2024-05-27 21:43:40 +0800

    5-27

---
+++ sugical_robot/mapping_strategy/derive_x.m
@@ -0,0 +1,7 @@
+t_3 = pi/6;
+t_4 = pi/6;
+ks_2 = acos(cos(t_4) * cos(t_3));
+phi_2 = acos( (cos(t_4) * sin(t_3))/ sin(ks_2));
+
+x_1 = acos(cos(phi_2) * cos(t_3)/cos(ks_2));
+x_2 = pi/2 - acos(cos(t_3) * sin(phi_2));
